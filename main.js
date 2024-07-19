b9b7996935c998790fba550edd0a7620fa0cd614
Albert Celestino <162110077+1lbert@users.noreply.github.com>
2024-07-19 16:51:04 +0800

    initial commit_homepage

---
+++ main.js
@@ -0,0 +1,9 @@
+const slideOutMenu = document.querySelector(".slideout-menu")
+
+function showMenu() { 
+    slideOutMenu.style.right = '0'
+}
+function closeMenu() { 
+    slideOutMenu.style.right = '-250px'
+}
+
